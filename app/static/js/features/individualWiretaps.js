31ab2451385d8ff79b2e98db6152ebab3de7f0f3
iFraG01 <francescogarofalo2001@gmail.com>
2023-10-27 09:45:25 +0200

    dimostrazione grafo

--- app/static/js/features/individualWiretaps.js
+++ app/static/js/features/individualWiretaps.js
@@ -9,13 +9,52 @@ window.onload = function () {
   createGraph();
 }
 
-function createGraph() {
-  let cy = cytoscape({
-    container: document.querySelector('.cyContent'),
-    elements: [
-      { data: { id: 'a' } },
-      { data: { id: 'b' } },
-      { data: { id: 'ab', source: 'a', target: 'b' } }
-    ]
-  });
+function createGraph(){
+    let cy = cytoscape({
+        container: document.querySelector('.cyContent'),
+        elements: [
+          // Definizione dei nodi
+          { data: { id: 'a' } },
+          { data: { id: 'b' } },
+          { data: { id: 'c' } },
+          { data: { id: 'd' } },
+          { data: { id: 'e' } },
+          { data: { id: 'f' } },
+          { data: { id: 'g' } },
+          { data: { id: 'h' } },
+          { data: { id: 'i' } },
+
+          // Definizione degli archi orientati
+          { data: { id: 'ab', source: 'a', target: 'b' } },
+          { data: { id: 'bc', source: 'b', target: 'c' } },
+          { data: { id: 'cd', source: 'c', target: 'd' } },
+          { data: { id: 'de', source: 'd', target: 'e' } },
+          { data: { id: 'ef', source: 'e', target: 'f' } },
+          { data: { id: 'fg', source: 'f', target: 'g' } },
+          { data: { id: 'gh', source: 'g', target: 'h' } },
+          { data: { id: 'hi', source: 'h', target: 'i' } },
+          { data: { id: 'ia', source: 'i', target: 'a' } }
+        ],
+        style: [ // Stile dei nodi e degli archi
+          {
+            selector: 'node',
+            style: {
+              'background-color': '#66CCFF',
+              'label': 'data(id)'
+            }
+          },
+          {
+            selector: 'edge',
+            style: {
+              'width': 2,
+              'line-color': '#333'
+            }
+          }
+        ],
+        layout: {
+          name: 'circle'
+        }
+      });
+      
+    console.log(cy.container());
 }
