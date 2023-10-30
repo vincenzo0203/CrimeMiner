f09d755320f3085edf7dc4fbab301773532854af
vincenzo0203 <v.cerciello1@studenti.unisa.it>
2023-10-30 11:36:01 +0100

    Request Mapping implementato

--- app/Views/IndividuoView.py
+++ app/Views/IndividuoView.py
@@ -6,7 +6,7 @@ from typing import List
 from app.Neo4jConnection import Neo4jDriver
 from django_request_mapping import request_mapping
 
-
+@request_mapping("/individuo")
 class IndividuoView(View):
 
     template_name = 'template_name.html'
@@ -14,30 +14,9 @@ class IndividuoView(View):
     def __init__(self):
         super().__init__()
         self.individuo_repository: IndividuoRepository = IndividuoRepository()
-        
-    def get(self, request):
-        #action = request.GET.get('action')
-        action = 'getIndividuoByName'
-        if action == 'findAll':
-            return self.find_all()
-        elif action == 'getIndividuoByName':
-            name = request.GET.get('name', 'Stranger')
-            return self.get_individuo_by_name(name)
-        elif action == 'getCloseness':
-            return self.get_closeness()
-        elif action == 'getDiameter':
-            return self.get_diameter()
-        elif action == 'getProj':
-            return self.get_proj()
-        elif action == 'getCustomProj':
-            t1 = request.GET.get('t1')
-            t2 = request.GET.get('t2')
-            return self.get_custom_proj(t1, t2)
-        else:
-            return JsonResponse({"error": "Invalid action"}, status=400)
 
-    @request_mapping("/findall/")
-    def find_all(self) -> JsonResponse:
+    @request_mapping("/findall/", method="get")
+    def find_all(self, request) -> JsonResponse:
         individuo_list = self.individuo_repository.find_all()
         return JsonResponse({"result": individuo_list})
 
--- app/urls.py
+++ app/urls.py
@@ -1,18 +1,29 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from app.Views.IndividuoView import IndividuoView
+
 
 from django_request_mapping import UrlPattern
 
 
-urlpatterns =  UrlPattern()
-urlpatterns.register(IndividuoView)
+urlpattern = UrlPattern()
+#Registrazione delle view del backend
+urlpattern.register(IndividuoView)
+
+
 
 urlpatterns = [
     path('', views.app),
     path('chiamate_individui', views.individualWiretaps),
-    path('/individuo', IndividuoView, name='individuo')
+
+    #Path per la registrazione di tutte le View del Backend funzionante con request mapping. Si crea una classe view, si definiscono le request mapping in questa classe creata
+    #e si registra la view creata in urlpattern
+    path('', include(urlpattern)) 
+    
 ]
+
+
+
 
 
 
